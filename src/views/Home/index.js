d53287280a4bf8d75eecf4db8ee6fb2016bdbb02
liyaocool <crazyliyao@163.com>
2019-05-10 00:29:01 +0800

    更新图标

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, Button, Platform, StyleSheet } from 'react-native';
+import Icon from "react-native-vector-icons/Ionicons"
 
 export default class HomeScreen extends Component {
   constructor(props) {
@@ -33,6 +34,7 @@ export default class HomeScreen extends Component {
       <View style={styles.Container}>
         <Text>首页</Text>
         <Text>计数: {this.state.count}</Text>
+        <Icon name="ios-settings" size={25} color="blue" />
         <Button
           title="去详情"
           onPress={() => {
