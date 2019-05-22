6c584ef51779c8971ef6c499848bb6172a401322
liyaocool <crazyliyao@163.com>
2019-05-22 18:11:23 +0800

    待完善二级页面取消底部导航

--- navigation/BottomNavigator.js
+++ navigation/BottomNavigator.js
@@ -1,58 +1,85 @@
-import React from 'react';
-import {createBottomTabNavigator} from 'react-navigation'
-import Icon from "react-native-vector-icons/Ionicons"
-import Home from '../views/Home'
-import HomeStack from './HomeStack'
-import MineStack from './MineStack'
-import MyPlugins from '../views/MyPlugins'
-import MyStyles from '../views/MyStyles'
-import Mine from '../views/Mine'
+import React, { Component } from "react";
+import { createBottomTabNavigator, BottomTabBar } from "react-navigation";
+import { View, Text, Button, StyleSheet, ScrollView } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+import HomeStack from "./HomeStack";
+import MineStack from "./MineStack";
+import MyPlugins from "../views/MyPlugins";
+import MyStyles from "../views/MyStyles";
+
+class CustomBottomComponent extends Component {
+  constructor() {
+    super();
+    this.state = { visible: false };
+  }
+  render() {
+    const {state} = this.props.navigation
+    return (
+      <View style={ state.routeName == 'HomePage' ? styles.noBottom : ''}>
+        <Text>{state.routeName}</Text>
+        <BottomTabBar {...this.props} />
+      </View>
+    );
+  }
+}
 
 const BottomNavigator = createBottomTabNavigator(
   {
     HomeStack: {
-    screen: HomeStack,
-    navigationOptions: {
-      tabBarLabel: '首页',
-      tabBarIcon:({focused, tintColor})=>(
-        <Icon name="ios-home" size={25} style={{color: tintColor}} />
-      )
+      screen: HomeStack,
+      navigationOptions: {
+        tabBarLabel: "首页",
+        tabBarIcon: ({ focused, tintColor }) => (
+          <Icon name="ios-home" size={25} style={{ color: tintColor }} />
+        )
+      }
+    },
+    MyPlugins: {
+      screen: MyPlugins,
+      navigationOptions: {
+        tabBarLabel: "插件",
+        tabBarIcon: ({ focused, tintColor }) => (
+          <Icon name="logo-javascript" size={25} style={{ color: tintColor }} />
+        )
+      }
+    },
+    MyStyles: {
+      screen: MyStyles,
+      navigationOptions: {
+        tabBarLabel: "样式",
+        tabBarIcon: ({ focused, tintColor }) => (
+          <Icon name="logo-css3" size={25} style={{ color: tintColor }} />
+        )
+      }
+    },
+    MineStack: {
+      screen: MineStack,
+      navigationOptions: {
+        tabBarLabel: "我的",
+        tabBarIcon: ({ focused, tintColor }) => (
+          <Icon name="logo-css3" size={25} style={{ color: tintColor }} />
+        )
+      }
     }
   },
-  MyPlugins: {
-    screen: MyPlugins,
-    navigationOptions: {
-      tabBarLabel: '插件',
-      tabBarIcon:({focused, tintColor})=>(
-        <Icon name="logo-javascript" size={25} style={{color: tintColor}}/>
-      )
-    }
-  },
-  MyStyles: {
-    screen: MyStyles,
-    navigationOptions: {
-      tabBarLabel: '样式',
-      tabBarIcon:({focused, tintColor})=>(
-        <Icon name="logo-css3" size={25} style={{color: tintColor}}/>
-      )
-    }
-  },
-  MineStack: {
-    screen: MineStack,
-    navigationOptions: {
-      tabBarLabel: '我的',
-      tabBarIcon:({focused, tintColor})=>(
-        <Icon name="logo-css3" size={25} style={{color: tintColor}}/>
-      )
-    }
-  },
-},
   {
+    tabBarComponent: props => <CustomBottomComponent {...props} />,
     tabBarOptions: {
-      activeTintColor:'blue',
-      showIcon: true
+      // activeTintColor: "red"
     }
   }
 );
 
-export default BottomNavigator
+const styles = StyleSheet.create({
+  test: {
+    // alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'red',
+  },
+  noBottom: {
+    width: 0,
+    height: 0,
+  }
+})
+
+export default BottomNavigator;
